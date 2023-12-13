536b9008e8adf73fc84b0225fece8a6c77f198c7
Isaacson Tyimanda <144861805+isaacsontyimanda@users.noreply.github.com>
2023-12-13 14:01:12 +0100

    estudando js

--- aula11/ex08.js
+++ aula11/ex08.js
@@ -1,2 +1,6 @@
-let vel = 60.5
-console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+let vel = 10
+console.log(`A velocidade do seu carro é de 🚗${vel}Km/h`)
+if(vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida⛔. Multado!`)
+}
+console.log(`Dirija sempre usando o sinto de segurança😊.`)
---
+++ aula11/ex09.js
@@ -0,0 +1,8 @@
+let país = 'Angola'
+console.log(`Você vive em ${país}`)
+if(país != 'Angola') {
+    console.log('Você é Estrangeiro')
+}
+else {
+    console.log('Você é Angolano')
+}
