5ae4c4f2a7ea59f6f34448dfd5bebcf7c8a08dfd
Isaacson Tyimanda <144861805+isaacsontyimanda@users.noreply.github.com>
2024-01-05 13:47:48 +0100

    exercicio de hora

--- aula012/ex014/intera.js
+++ aula012/ex014/intera.js
@@ -1,25 +1,20 @@
 function carregar() {
     let titu = document.querySelector('h1#titu')
     let msg = document.querySelector('div#msg')
-    let imag = document.querySelector('img#imagem')
+    let img = document.getElementById('imagem')
     let data = new Date()
     let hora = data.getHours()
-    if (hora <= 11) {
+    msg.innerText = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
         titu.innerText = `Bom Dia!`
-        msg.innerText = `Agora são ${hora} horas.`
-        imag.innerHTML = `<img id="imagem" src="img/dia.png" alt="foto do dia">`
+        img.src = 'dia.png'
     }
-    else if (hora <= 17) {
+    else if (hora >= 12 && hora < 18) {
         titu.innerText = `Boa Tarde!`
-        msg.innerText = `Agora são ${hora} horas.`
-        imag.innerHTML = `<img id="imagem" src="img/tarde.png" alt="foto do dia">`
-    }
-    else if (hora => 18) {
-        titu.innerText = `Boa Noite!`
-        msg.innerText = `Agora são ${hora} horas.`
-        imag.innerHTML = `<img id="imagem" src="img/noite.png" alt="foto do dia">`
+        img.src = 'tarde.png'
     }
     else {
-        msg.innerText = `[ERRO] Infelizmente essa hora que inseriste não existe!`
+        titu.innerText = `Boa Noite!`
+        img.src = 'noite.png'
     }
 }
