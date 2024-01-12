674583da4e6d7fe0d4e52f9a747d35b035409986
Isaacson Tyimanda <144861805+isaacsontyimanda@users.noreply.github.com>
2024-01-12 14:00:16 +0100

    terminado ex015

--- aula012/ex015/exerc02.js
+++ aula012/ex015/exerc02.js
@@ -16,42 +16,42 @@ function verificar() {
             gênero = 'Homem'
             if (idade >= 0 && idade < 10) {
                 //Criança
-                img.setAttribute('src', 'menino.png')
+                img.setAttribute('src', 'img/menino.png')
             }
-            else if (idade <21) {
+            else if (idade < 21) {
                 //Jovem
-                img.setAttribute('src', 'homem-jovem.png')
+                img.setAttribute('src', 'img/homem-jovem.png')
             }
             else if (idade < 50) {
                 //Adulto
-                img.setAttribute('src', 'homem-adulto.png')
+                img.setAttribute('src', 'img/homem-adulto.png')
             }
             else {
                 //Idoso
-                img.setAttribute('src', 'homem-idoso.png')
+                img.setAttribute('src', 'img/homem-idoso.png')
             }
         }
         else if (fsex[1].checked) {
             gênero = 'Mulher'
             if (idade >= 0 && idade < 10) {
                 //Criança
-                img.setAttribute('src', 'menina.png')
+                img.setAttribute('src', 'img/menina.png')
             }
             else if (idade <21) {
                 //Jovem
-                img.setAttribute('src', 'mulher-jovem.png')
+                img.setAttribute('src', 'img/mulher-jovem.png')
             }
             else if (idade < 50) {
                 //Adulto
-                img.setAttribute('src', 'mulher-adulta.png')
+                img.setAttribute('src', 'img/mulher-adulta.png')
             }
             else {
                 //Idoso
-                img.setAttribute('src', 'mulher-idosa')
+                img.setAttribute('src', 'img/mulher-idosa.png')
             }
         }
         res.style.textAlign = 'center'
         res.innerText = `Detectamos ${gênero} com ${idade} anos de idade.`
-        res.appendChild('img')
+        res.appendChild(img)
     }
 }
