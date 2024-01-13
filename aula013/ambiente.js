5124455c58061ed3b7a3dcdf65b685ef8fc1fbe8
Isaacson Tyimanda <144861805+isaacsontyimanda@users.noreply.github.com>
2024-01-13 01:11:00 +0100

    Estudando repetições parte 2

--- aula013/ambiente.js
+++ aula013/ambiente.js
@@ -1,12 +1,19 @@
-let c = 1
+for (let c=1; c <= 5; c++) {
+    console.log(`Passo ${c}`)
+}
+
+/*let c = 1*/
+//Os métodos recomendados para repetição são: while(){} ou o do {} while().
+
 /*do {
     console.log(`Passo ${c}`)
     c++
 } while (c <= 5)*/
 
-while (c <= 5) {
-    console.log(`Passo ${c`)
-}
+/*while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++
+}*/
 
 //Pior solução para criar repetições ou laços.
 /*console.log(`Passo ${c}`)
