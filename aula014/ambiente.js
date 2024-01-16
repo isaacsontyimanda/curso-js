1e10a894066ad97e8cd3bae8131cef9f3324f047
Isaacson Tyimanda <144861805+isaacsontyimanda@users.noreply.github.com>
2024-01-16 15:49:42 +0100

    Estudando Repetiçoes by cursoemvideo

--- aula014/ambiente.js
+++ aula014/ambiente.js
@@ -1,3 +1,16 @@
-for (let c=1; c <= 5; c++) {
-    console.log(`Passo ${c}`)
+let c = 1
+/*while (c <= 10) {
+    console.log(`Nota: ${c}`) //Método while(){}
+    c++
+}*/
+
+/*do {
+    console.log(`Nota: ${c}`) //Método do{} while()
+    c++
+}while (c <= 10)*/
+
+console.log('Vai começar...')
+for (let c = 1; c <= 4; c++) { //Método for(){}
+    console.log(`Nota: ${c}`)
 }
+console.log('FIM!')
