2a2de854692a34761c0eeebfc0fa174b04c35622
Isaacson Tyimanda <144861805+isaacsontyimanda@users.noreply.github.com>
2024-01-20 11:53:00 +0100

    fazendo o exercício de repetição 01

---
+++ aula014/ex016/ex03.js
@@ -0,0 +1,5 @@
+let inicio = document.querySelector('input#inicio')
+let fim = document.querySelector('input#fim')
+let passo = document.querySelector('input#passo')
+let cont = document.querySelector('div#cont')
+
