8d9574c6dce9c19b1560750a6cefe6f53e539eca
Isaacson Tyimanda <144861805+isaacsontyimanda@users.noreply.github.com>
2024-01-21 15:19:26 +0100

    fazendo o exercício 03

--- aula014/ex016/ex03.js
+++ aula014/ex016/ex03.js
@@ -1,5 +1,20 @@
-let inicio = document.querySelector('input#inicio')
+function contar() {
+let ini = document.querySelector('input#inicio')
 let fim = document.querySelector('input#fim')
 let passo = document.querySelector('input#passo')
 let cont = document.querySelector('div#cont')
 
+if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    alert('ERRO! dados faltando, verifique e tente novamente...')
+}
+else {
+    alert('Seus dados estão [OK], seu programa seguirá...')
+    cont.innerHTML = 'Contando...'
+    let i = Number(ini.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+    for (let c = i; c <= f; c += p) {
+        cont.innerHTML += `${c}`
+    }
+}
+}
