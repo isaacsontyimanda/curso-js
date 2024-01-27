625296b59dd2630201c3483e7e80ce3244001635
Isaacson Tyimanda <144861805+isaacsontyimanda@users.noreply.github.com>
2024-01-27 10:42:47 +0100

    Agora feito por mim

--- aula014/ex016/ex03.js
+++ aula014/ex016/ex03.js
@@ -2,30 +2,26 @@ function contar() {
     let inicio = document.querySelector('input#inicio')
     let fim = document.querySelector('input#fim')
     let passo = document.querySelector('input#passo')
-    let result = document.querySelector('div#cont')
+    let result = document.querySelector('div#result')
 
     let i = Number(inicio.value)
     let f = Number(fim.value)
     let p = Number(passo.value)
 
-    if (f <= 0 ) {
-        alert('[ERRO] dados inválidos, verifique os dados inseridos e tente novamente...')
-    }
-    else {
-        result.innerHTML = 'Contando: <br>'  
-    }
+    result.innerHTML = 'Contando: <br>'
     if (p <= 0) {
         alert('Passo inválido, considerando passo: 1')
         p = 1
     }
-    if (f > i) { //Contagem Crescente
-        for (let c = i ; c <= f ; c += p ) {
-            result.innerHTML += `\u{1F449} ${c} `} 
-    }
-    if (i > f) { //Contagem Decrescente
-        for (let c = i; c >= f; c-= p) {
+    if (f > i) {
+        for (let c = i; c <= f; c += p) {
             result.innerHTML += `\u{1F449} ${c} `
         }
     }
-        result.innerHTML += `\u{1F3C1}`
+    if (i > f) {
+        for (let c = i; c >= f; c -= p) {
+            result.innerHTML += `\u{1F449} ${c} `
+        }
     }
+    result.innerHTML += '\u{1F3C1}'
+}
